dedddbc9ca8cdee305e7d4f0debe5ae03e9166c1
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-07-11 22:27:47 +0000

    added short url/route parameter page

--- express_server.js
+++ express_server.js
@@ -14,7 +14,6 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -27,7 +26,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/:id", (req, res) => {
 
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
