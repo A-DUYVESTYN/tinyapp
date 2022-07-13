2c8532656b75500a568f0617a233b50ccfb02d36
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-07-13 22:09:19 +0000

    added login, register, user authentication

--- express_server.js
+++ express_server.js
@@ -1,35 +1,51 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-
-/////////////////////////////////////////////////////////////
+const morgan = require('morgan')
+///////////////////////////////////////////////////////////
 // Configuration
-/////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
-
+///////////////////////////////////////////////////////////
+// Databases
+///////////////////////////////////////////////////////////
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-/////////////////////////////////////////////////////////////
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+///////////////////////////////////////////////////////////
 // Middleware
-/////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 
 // body-parser library will convert the request body from a Buffer into string that we can read
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-/////////////////////////////////////////////////////////////
-// Routes
-/////////////////////////////////////////////////////////////
+app.use(morgan('dev'));
 
+///////////////////////////////////////////////////////////
+// GET Routes
+///////////////////////////////////////////////////////////
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase, 
-    username: req.cookies["username"] // access cookie
+    user: users[req.cookies["user_id"]] // access cookie
   };
   res.render('urls_index', templateVars);
 });
@@ -48,7 +64,7 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"] // access cookie
+    user: users[req.cookies["user_id"]] // access cookie
   };
   res.render("urls_new", templateVars);
 });
@@ -58,7 +74,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { 
     id: req.params.id, 
     longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"]  // access cookie
+    user: users[req.cookies["user_id"]]  // access cookie
   }
   res.render("urls_show", templateVars);
 });
@@ -68,6 +84,27 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+app.get("/register", (req, res) => {
+
+  console.log(req.cookies["user_id"])
+  const templateVars = { 
+    user: users[req.cookies["user_id"]] // access cookie
+  };
+  console.log(templateVars)
+  res.render(`urls_register`, templateVars)
+});
+
+app.get("/login", (req, res) => {
+
+  const templateVars = { 
+    user: users[req.cookies["user_id"]] // access cookie
+  };
+  res.render(`urls_login`, templateVars)
+});
+
+///////////////////////////////////////////////////////////
+// POST Routes
+///////////////////////////////////////////////////////////
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log POST request body to console
   randomstr = generateRandomString()
@@ -90,26 +127,60 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  console.log(req.body) //log username to console for debug
-  res.cookie("username", req.body["username"])
+  const currentUser = userLookupByEmail(req.body["email"])
+  if (currentUser === null) {
+    res.status(403).send('e-mail cannot be found.')
+    return
+  } else if (users[currentUser].password !==  req.body["password"]) {
+    res.status(403).send('incorrect e-mail or password')
+    return
+  } 
+  res.cookie("user_id", users[currentUser].id)  
+  console.log("cookie:", req.cookies["user_id"]) // this returns undefined??
   res.redirect(`/urls`)
-  console.log(req.cookies["username"]) // this logs the previous username for some reason.. res.cookie is async??
+  
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username")
+  res.clearCookie("user_id")
   res.redirect(`/urls`)
 });
 
-/////////////////////////////////////////////////////////////
+app.post("/register", (req, res) => {
+  if (req.body["email"] === '') {
+    res.status(400).send('Registration must include a valid email.')
+    console.log(users)
+    return
+  } else if (req.body["password"] === '') {
+    res.status(400).send('Registration must include a valid password.')
+    console.log(users)
+    return
+  } else if (userLookupByEmail(req.body["email"]) !== null) {
+    res.status(400).send('This email address is already registered.')
+    console.log(users)
+    return
+  }
+  newRandId = generateRandomString()
+  users[newRandId] = newRandId
+  users[newRandId] = {
+    id: newRandId,
+    email: req.body["email"],
+    password: req.body["password"],
+  }
+  res.cookie("user_id", users[newRandId].id)
+  console.log(users) //log user database to console for debug
+  res.redirect(`/urls`)
+});
+
+///////////////////////////////////////////////////////////
 // Server Listener
-/////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-/////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 // Functions
-/////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 
 const generateRandomString = function() {
   let length = 6;
@@ -119,4 +190,13 @@ const generateRandomString = function() {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
  return result;
+}
+
+const userLookupByEmail = function(findThisEmail) {
+  for (const user in users) {
+    if (users[user].email === findThisEmail) {
+      return user
+    }
+  }
+  return null
 }
